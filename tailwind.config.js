6122d28c635b428b7796ee6c4c3328fe053ca152
k3mosh <cocsmurftzy@gmail.com>
2025-02-21 12:22:57 +0800

    test

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+
+    content: ["./src/**/*.{html,js,jsx,ts,tsx}"], // Adjust paths to match your project
+  
+    theme: {
+  
+      extend: {
+  
+        colors: {
+  
+          primary: "#007bff", // Custom color example
+  
+        },
+  
+        fontFamily: {
+  
+          baloo2: ["Baloo 2", "sans-serif"],
+  
+        },
+        
+  
+      },
+  
+    },
+  
+    plugins: [],
+  
+  };
